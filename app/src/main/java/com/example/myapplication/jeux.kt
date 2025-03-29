5f63435e53bcf073ac54c3022d3f532db8b41660
Abolfazl47903 <a.alahdad123@gmail.com>
2025-03-29 17:02:22 +0100

    Message

--- app/src/main/java/com/example/myapplication/jeux.kt
+++ app/src/main/java/com/example/myapplication/jeux.kt
@@ -1,4 +1,32 @@
 package com.example.myapplication
 
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
 class jeux {
+
+    // attributs
+    private var score : Int = 0
+    private var vie : Int = 3
+    private var niveau_actuel : Int = 1
+
+    //propriétés publiques
+    var record : Int = 0
+    var vie_supp : Int = 0
+
+    //méthodes
+    fun verifier_fin_niveau(){
+
+    }
+    fun game_over(){
+
+    }
+    fun restart_game(){
+
+    }
+
+
 }
