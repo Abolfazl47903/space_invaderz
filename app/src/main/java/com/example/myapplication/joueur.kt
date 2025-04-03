b07cef60585c8c82101019e3766e0b952a96d361
yabo0008 <yabokor@hotmail.com>
2025-04-03 18:50:06 +0200

    Message

--- app/src/main/java/com/example/myapplication/joueur.kt
+++ app/src/main/java/com/example/myapplication/joueur.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.PointF
+import android.graphics.Color
 
 class joueur (var fuseeLongueur : Float, var fuseeHauteur : Float, val vue : jeux){ //Pour l'instant la fusée est un rectangle
     val fuseePaint =Paint()
@@ -12,7 +13,7 @@ class joueur (var fuseeLongueur : Float, var fuseeHauteur : Float, val vue : jeu
         fuseePaint.strokeWidth = largeur * 1.5f
         canvas.drawLine(0f,vue.screenHeight/4,finFusee.x,finFusee.y,fuseePaint)
     }
-    fun finfusee(hauteur: Float){ //Méthode servant à initialiser la fin du canon
+    fun finfusee(hauteur: Float){ //Méthode servant à initialiser la fin de la fusée
         finFusee.set(fuseeLongueur, hauteur)
     }
     fun alignement(amplitude : Double){ //Au click pour diriger les missiles
--- app/src/main/java/com/example/myapplication/missile.kt
+++ app/src/main/java/com/example/myapplication/missile.kt
@@ -1,4 +1,24 @@
 package com.example.myapplication
 
-class missile {
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.PointF
+import android.graphics.Color
+
+class missile (var vue : jeux, val alien : Aliens){
+    var missile = PointF()
+    var missileVitesse =0f
+    var missileVitesseX =0f
+    var missileVitesseY =0f
+    var missileOnScreen =true
+    var missileTaille =0f
+    var missilePaint = Paint()
+
+    init{
+        missilePaint.color = Color.WHITE
+    }
+    fun lancer(amplitude : Double){
+        missile.x = vue.screenWidth/2f
+        missile.y = missileTaille
+    }
 }
