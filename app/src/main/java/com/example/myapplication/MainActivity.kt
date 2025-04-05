5dc8a412766066986b950125d41e61bb6d028d4f
Abolfazl47903 <a.alahdad123@gmail.com>
2025-04-05 21:46:05 +0200

    sdfjkl

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,13 +2,16 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var start : Button(R.id.Start)
+    lateinit var gauche : Button(R.in.gauche)
+    lateinit var droite : Button(R.in.droite)
     lateinit var Jeux : jeux
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +23,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val Jeux = jeux()
+        start = findViewById(R.id.start)
         val AlienView=AlienView(this)
         setContentView(AlienView)
+
+
+
+        start.setOnClickListener{
+            Jeux.start_game()
+        }
     }
     /*override fun Pause() {
         super.Pause()
--- app/src/main/java/com/example/myapplication/jeux.kt
+++ app/src/main/java/com/example/myapplication/jeux.kt
@@ -11,6 +11,8 @@ import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.FragmentActivity
 import org.xmlpull.v1.XmlPullParser
 import android.content.Context
+import android.widget.Button
+
 
 class jeux @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = 0): SurfaceView(context, attributes,defStyleAttr), SurfaceHolder.Callback, Runnable{
 
@@ -22,59 +24,36 @@ class jeux @JvmOverloads constructor (context: Context, attributes: AttributeSet
     //propriétés publiques
     var record : Int = 0
     var vie_supp : Int = 1
-    var dessin = false
+    var interfacee = false
     var gameover = false
     val activite = context as FragmentActivity
     lateinit var thread :Thread
     var screenWidth = 0f
     var screenHeight = 0f
+    lateinit var gauche : Button(R.in.gauche)
+    lateinit var droite : Button(R.in.droite)
+
 
     //méthodes
     fun start_game(){
-        dessin = false
-        val monstres = mutableListOf<Aliens>() // créer une liste modifiable dans la classe Aliens pour y stocker les positions et type de monstres.
-        val parcourir = activite.resources.getXml(R.xml.levels) // prends les données qui sont dans le fichier levels.xml et les stock dans parcourir.
-        var x = 0  //j'initialise les positions en x et y et le type de monstre
-        var y = 0
-        var type = ""
-        var etape = parcourir.eventType
-        /* on crée une variable etape qui nous permettera de savoir ou est-ce qu'on se situe
-        dans parcourir et SURTOUT IL NOUS PERMET DE CONNAITRE C'EST QUOI LE TYPE DE L'ENDROIT
-        OU ON SE SITUE. par exemple etape peut nous dire qu'on est au début ou à la fin d'un document.
-        Il peut aussi nous dire "ok la on ouvre une balise" ou bien "la on ferme une balise" , etc.
-        var etape = parcourir.eventType  initialise etape à "on est au début du document".
-         */
-        while (etape != XmlPullParser.END_DOCUMENT) {// on fait un while jusqu'à la fin du document avec toute les positions
-
-
-            when (etape){
-                XmlPullParser.START_TAG -> { // quand etape nous indique qu'on ouvre une balise, d'où le START_TAG
-                    //On stock toutes les informations qu'on a besoin
-                    x = parcourir.getAttributeIntValue(null, "x",0)
-                    y = parcourir.getAttributeIntValue(null, "y",0)
-                    type = parcourir.getAttributeValue(null, "type")
-                }
-                XmlPullParser.END_TAG -> { // quand etape nous indique qu'on ferme une balise, d'où le END_TAG
-                    monstres.add(Aliens(x, y, type))// on ajoute les infos dans la classe Aliens dans une liste monstres, qu'on avait créer un début de la fonction
-                }
-            }
-            if (monstres.size == 50) {
-                /* vu qu'on commence au niveau 1 on veut que cette fonction prennent en compte que les
-                monstres du niveau 1, logique on va pas afficher les autres niveau aussi. Ducoup on fait
-                un if avec le nombre de monstre == 50 car quand il y a 50 monstres stocké dans la liste
-                alors on break le while (il y a 50 monstres au niveau 1)
-                 */
-                break
-            }
-            etape = parcourir.next()
+        val joueur = joueur()
+        joueur.dessin(this)
+        val AlienView=AlienView(this)
+        setContentView(AlienView)
+        gauche = findViewById(R.id.gauche)
+        droite = findViewById(R.id.droite)
+        gauche.setOnClickListener{
+            joueur.deplacement()
         }
-
+        droite.setOnClickListener{
+            joueur.deplacement()
+        }
     }
     fun verifier_fin_niveau(){
 
     }
     fun game_over(){
-        dessin = false //Sert à montrer comment est l'écran. Si false alors on le voit pas si true on le voit
+        interfacee = false //Sert à montrer comment est l'écran. Si false alors on le voit pas si true on le voit
         prestation(R.string.win) //Méthode pour savoir si on a gagné ou perdu
         gameover = true //Si true alors on a perdu
     }
@@ -102,14 +81,14 @@ class jeux @JvmOverloads constructor (context: Context, attributes: AttributeSet
                 resultat.setCancelable(false)
                 resultat.show(feet,"dialog")
             }
-        )
+        )   
     }
     fun pause() { //Si on veut faire une pause en quittant l'appli
-        dessin = false
+        interfacee = false
         thread.join() //Les thread servent quand on ouvre plusieurs application (voir cours)
     }
     fun reprendre() { //Si on veut reprendre le jeu
-        dessin = true
+        interfacee = true
         thread = Thread(this)
         thread.start()
     }
