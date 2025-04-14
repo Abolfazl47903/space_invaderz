15e60b3d6a11da72303b738dd113f25d7c78f00f
kliu002 <liukevin45@gmail.com>
2025-04-14 22:54:27 +0200

    Update Aliens.kt

--- app/src/main/java/com/example/myapplication/Aliens.kt
+++ app/src/main/java/com/example/myapplication/Aliens.kt
@@ -4,57 +4,49 @@ import android.annotation.SuppressLint
 import android.graphics.RectF
 
 abstract class Aliens(
-    var vitesseX: Int, // attributs privés
+    var vitesseX: Int,// qui sont les attributes privée
     var vitesseY: Int,
     var vitesseTir: Int,
     var taille: Int,
-    var alienDistance: Float, var alienDebut: Float, var alienFin: Float, var alienVitesseInitiale: Float, var width: Float, var view: jeux,
+    var alienDistance: Float, var alienDebut: Float, var alienFin: Float, var alienVitesseInitiale: Float, var width: Float, var view: AlienView,
     var state: UpdateState? = null
-) {
-    @SuppressLint("NotConstructor") // juste pour dire que ceci n'est pas un constructeur
-    fun actionAliens(): String {
+){
+    @SuppressLint("NotConstructor")// juste pour dire que ceci n'est pas un constructeur
+    fun actionAliens() :String{
         return "tout les aliens se déplace de manière horizontale en x et verticalement en y "
+
     }
-
     var alien = RectF(alienDistance, alienDebut, alienDistance + width, alienFin)
-
-    // Utiliser des propriétés avec des getters et setters personnalisés
-    var vitesseXValue: Int
-        get() = vitesseX
-        set(value) {
-            vitesseX = value
-        }
-
-    var vitesseYValue: Int
-        get() = vitesseY
-        set(value) {
-            vitesseY = value
-        }
-
-    protected fun afficherTaille(): Int { // cette méthode permet d'afficher la taille des aliens
+    /*comme les attributs de Aliens sont privée et qu'on doit qu'a même les utilisé dans les sous classe crabes
+    calmar, poulpe on doit faire des getter et setter
+     */
+    protected fun getvitesseX()=vitesseX
+    protected fun setvitesseX(value: Int){
+        vitesseX=value
+    }
+    protected fun getvitesseY()=vitesseY
+    protected fun setvitesseY(value: Int){
+        vitesseY=value
+    }
+    protected fun afficherTaille():Int{ //cette méthode permet d'afficher la taille des aliens
         return taille
     }
-
-    abstract fun shot() // qui seront redéfinies dans les sous-classes crabes, poulpes et calmar
-
+    abstract fun shot()  // qui seront rédéfinit dans les sous classes crabes , poulpes et calmar
+    //abstract fun update()
     fun changeDirection() {
-        // Votre code ici
+
     }
+    fun FinDujeu(){ // mais la function findujeux n'est pas obligé dêtre abstract
 
-    fun finDuJeu() { // la fonction finDuJeu n'est pas obligée d'être abstract
-        // Votre code ici
     }
+    abstract  fun donnerPoint(): Int
 
-    abstract fun donnerPoint(): Int
-
-    fun detectChoc(missile: missile) {
+    fun detectchoc(missile : missile){
         if (missile.collision(1.2)) {
             state = AlienMort(this)
             state?.update()
         }
     }
 
-    fun donnerPoints() {
-        // Votre code ici
-    }
+
 }
