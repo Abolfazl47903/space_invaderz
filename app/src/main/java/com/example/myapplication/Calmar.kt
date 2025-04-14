c3c88844bf90fc4ab683c7bd88ff5bdb1f39cbc9
kliu002 <liukevin45@gmail.com>
2025-04-14 22:54:46 +0200

    Update Calmar.kt

--- app/src/main/java/com/example/myapplication/Calmar.kt
+++ app/src/main/java/com/example/myapplication/Calmar.kt
@@ -1,14 +1,31 @@
 package com.example.myapplication
+    class Calmar(
+        vitesseX: Int,
+        vitesseY: Int,
+        vitesseTir: Int,
+        taille: Int,
+        alienDistance: Float,
+        alienDebut: Float,
+        alienFin: Float,
+        alienVitesseInitiale: Float,
+        width: Float,
+        view: AlienView
+    ) : Aliens(
+        vitesseX,
+        vitesseY,
+        vitesseTir,
+        taille,
+        alienDistance,
+        alienDebut,
+        alienFin,
+        alienVitesseInitiale,
+        width,
+        view
+    ) {
+        override fun shot() {
+        }
 
-class Calmar(vitesseX: Int, vitesseY: Int, vitesseTir: Int, taille: Int,
-             alienDistance: Float, alienDebut: Float, alienFin: Float, alienVitesseInitiale: Float, width: Float, view: jeux)
-    : Aliens(vitesseX, vitesseY, vitesseTir, taille, alienDistance, alienDebut, alienFin, alienVitesseInitiale, width, view) {
-    override fun shot() {
-        TODO("Not yet implemented")
+        override fun donnerPoint(): Int {
+            return 1
+        }
     }
-
-
-    override fun donnerPoint() :Int{
-        return 1
-    }
-}
