6a127f5ad869a2f619f5a4ecd6d315e3e2cf3052
yabo0008 <yabokor@hotmail.com>
2025-04-17 22:24:54 +0200

    Update missileJoueur.kt

--- app/src/main/java/com/example/myapplication/missileJoueur.kt
+++ app/src/main/java/com/example/myapplication/missileJoueur.kt
@@ -1,8 +1,8 @@
 package com.example.myapplication
 
-class missileJoueur (
-    val joueur: joueur,
-    private val jeux: jeux) : UpdateState {
+class MissileJoueur (
+    val joueur: Joueur,
+    private val jeux: Jeux) : UpdateState {
     override fun update() {
         if (jeux.vie == 0){joueur.joueur.set(-100f, -100f, -100f, -100f)}
     }
