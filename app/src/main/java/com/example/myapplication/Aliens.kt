86119707e577735f6553e591bf7f7909191ae541
yabo0008 <yabokor@hotmail.com>
2025-04-17 22:21:03 +0200

    Update Aliens.kt

--- app/src/main/java/com/example/myapplication/Aliens.kt
+++ app/src/main/java/com/example/myapplication/Aliens.kt
@@ -3,14 +3,19 @@ import android.annotation.SuppressLint
 import android.graphics.RectF
 
 abstract class Aliens(
-    var vitesseX: Float,// qui sont les attributes privée
+    var vitesseX: Float,
     var vitesseY: Float,
     var vitesseTir: Int,
     var taille: Int,
-    var alienDistance: Float, var alienDebut: Float, var alienFin: Float, var alienVitesseInitiale: Float, var width: Float, var view: AlienView,
+    var alienDistance: Float,
+    var alienDebut: Float,
+    var alienFin: Float,
+    var alienVitesseInitiale: Float,
+    var width: Float,
+    var view: AlienView,
     var state: UpdateState? = null
 ){
-    @SuppressLint("NotConstructor")// juste pour dire que ceci n'est pas un constructeur
+    @SuppressLint("NotConstructor")// Juste pour dire que ceci n'est pas un constructeur
     var alien = RectF(alienDistance, alienDebut, alienDistance + width, alienFin)
     /*comme les attributs de Aliens sont privée et qu'on doit qu'a même les utilisé dans les sous classe crabes
     calmar, poulpe on doit faire des getter et setter
